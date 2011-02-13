50988ba0098ebc7e69aee18c6fe633d250e2e292
Alexey Skhodnensky <rtfm61@yandex.ru>
2011-02-14 02:21:06 +0300

    Removed default Rails page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+#return title on a per-page basis
+def title
+  base_title = "RoR TSA"
+  if @title.nil?
+    base_title
+  else
+    "#{base_title} | #{@title}"
+  end
+end
+
 end
